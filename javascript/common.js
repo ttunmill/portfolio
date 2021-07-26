9a4239b46653ae68847ac0d91316dfb5bf92f2ff
ttunmill <ttunmill@gmail.com>
2021-07-26 18:59:01 +0900

    project

--- javascript/common.js
+++ javascript/common.js
@@ -1,11 +1,24 @@
-$(document).on('mouseover', '.topMenu span', function () {
-    $('.dept02').slideDown(200);
+$(document).ready(function() {
+    var timer;
+    $(".topMenu").mouseenter(function() {
+        timer = setTimeout(function() {
+            $(".dep02").not(":animated")
+            .slideDown(500)
+        }, 300);
+    })
+    $(".dep01").mouseleave(function() {
+        clearTimeout(timer);
+        $(".dep02")
+        .slideUp(500)
+    });
 });
-$(document).on('mouseover', 'div', function () {
-    if (!$(this).hasClass('topMenu')) {
-        $('.dept02').slideUp(200);
-    }
-});
 
 
-
+// $(document).on('mouseover', '.topMenu span', function () {
+//     $('.dept02').slideDown(200);
+// });
+// $(document).on('mouseover', 'div', function () {
+//     if (!$(this).hasClass('topMenu')) {
+//         $('.dept02').slideUp(200);
+//     }
+// });
--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,29 @@
-$(function(){
-    $(".nav_menu li").click(function(){
-        $('ul:first',this).show();
-    }, function(){
-        $('ul:first',this).hide();
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        loop:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        center:true,
+        responsiveClass:true, 
+        responsive:{ 
+            0:{ items:1 }, 
+            600:{ items:2 }, 
+            1000:{ items:4 } 
+        }
+        
     });
-    $(".nav_menu>li:has(ul)>a").each( function() {
-        $(this).html( $(this).html()+' &or;' );
-    });
-    $(".nav_menu ul li:has(ul)")
-        .find("a:first")
-        .append("<p style='float:right;margin:-3px'>&#9656;</p>");
+    var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.owl-next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.owl-prev').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
 });
